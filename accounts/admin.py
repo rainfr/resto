07753eff7dde2a379ea9aaccfe781336330762ae
Jean Chevronnet <att4ck07@gmail.com>
2023-07-11 23:04:34 +0200

    add list_display to class

--- accounts/admin.py
+++ accounts/admin.py
@@ -4,6 +4,7 @@ from django.contrib.auth.admin import UserAdmin
 
 
 class CustomUserAdmin(UserAdmin):
+    list_display = ('email', 'username', 'first_name', 'last_name', 'is_active', 'is_staff', 'is_superadmin', 'role')
     filter_horizontal = ()
     list_filter = ()
     fieldsets = ()
