d4f4d89101c52c541f4af6bc722ead9532042f3a
Jean Chevronnet <att4ck07@gmail.com>
2023-07-11 22:58:52 +0200

    add correct user password field in the backend

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import User
+from django.contrib.auth.admin import UserAdmin
 
 
-admin.site.register(User)
+class CustomUserAdmin(UserAdmin):
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+
+
+admin.site.register(User, CustomUserAdmin)
