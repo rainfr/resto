a026ee2285c606276347629b4d54e51295e90ce6
Jean Chevronnet <att4ck07@gmail.com>
2023-07-12 16:27:05 +0200

    add media folder and backend

--- resto_main/settings.py
+++ resto_main/settings.py
@@ -127,6 +127,11 @@ STATICFILES_DIRS = [
     BASE_DIR / 'resto_main/static'   
 ]
 
+# Media files
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
