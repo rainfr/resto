4bdf8571618ed14ecc766221ceea9bdffac62dbe
Jean Chevronnet <att4ck07@gmail.com>
2023-08-10 21:00:34 +0200

    create templates

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,6 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def registerUser(request):
+    return HttpResponse('this is a test reg form')
