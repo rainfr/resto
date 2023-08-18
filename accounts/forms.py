4d0563d63a81742273baec1b64e173fc1528e2cf
Jean Chevronnet <att4ck07@gmail.com>
2023-08-19 01:29:48 +0200

    post method

--- accounts/forms.py
+++ accounts/forms.py
@@ -7,4 +7,4 @@ class UserForm(forms.ModelForm):
     confirm_password = forms.CharField(widget=forms.PasswordInput())
     class Meta:
         model = User
-        fields = ['first_name', 'last_name','username', 'email', 'phone_number', 'password']
+        fields = ['first_name', 'last_name', 'username', 'email', 'password']
--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,20 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import UserForm
+from .models import User
 
 def registerUser(request):
-    form = UserForm()
-    context = {
-        'form': form,
-    }
-    return render(request, 'accounts/registerUser.html', context)
+    if request.method == 'POST':
+        print(request.POST)
+        form = UserForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.role = User.CUSTOMER
+            user.save()
+            return redirect('registerUser')
+    else:
+        form = UserForm()
+        context = {
+            'form': form,
+        }
+        return render(request, 'accounts/registerUser.html', context)
