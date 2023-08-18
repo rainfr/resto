02d57df837ed7912b281220a1b2c613c16627b7f
Jean Chevronnet <att4ck07@gmail.com>
2023-08-19 01:55:47 +0200

    implement hashed password

--- accounts/views.py
+++ accounts/views.py
@@ -8,9 +8,23 @@ def registerUser(request):
         print(request.POST)
         form = UserForm(request.POST)
         if form.is_valid():
-            user = form.save(commit=False)
+            #Create the user using form
+            #password = form.cleaned_data['password']
+            #user = form.save(commit=False)
+            #user.role = User.CUSTOMER
+            #user.save()
+            
+            #Create the user using create_user method
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            username = form.cleaned_data['username']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            user = User.objects.create_user(first_name=first_name, last_name=last_name, username=username, email=email, password=password)
             user.role = User.CUSTOMER
             user.save()
+            print('User created')
+            
             return redirect('registerUser')
     else:
         form = UserForm()
