2b272e60e47fb42af08515d74168d4e9abe658af
Jean Chevronnet <att4ck07@gmail.com>
2023-08-20 13:31:43 +0200

    implment  django messages for non field messages

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from .forms import UserForm
 from .models import User
+from django.contrib import messages
 
 def registerUser(request):
     if request.method == 'POST':
@@ -22,6 +23,7 @@ def registerUser(request):
             user = User.objects.create_user(first_name=first_name, last_name=last_name, username=username, email=email, password=password)
             user.role = User.CUSTOMER
             user.save()
+            messages.success(request, 'Woow great. Your account has been created successfully.')
             return redirect('registerUser')
         else:
             print('invalid form')
