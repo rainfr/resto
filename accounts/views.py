1ad2ba5d658d8a4b1000d9e14dc9498f1769003a
rain <att4ck07@gmail.com>
2023-08-25 00:26:11 +0200

    activating the User

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,8 @@
 from django.contrib import messages, auth
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.tokens import default_token_generator
 from django.shortcuts import render, redirect
+from django.utils.http import urlsafe_base64_decode
 
 from vendor.forms import VendorForm
 from .forms import UserForm
@@ -89,7 +91,20 @@ def registerVendor(request):
 
 
 def activate(request, uidb64, token):
-    return
+    try:
+        uid = urlsafe_base64_decode(uidb64).decode()
+        user = User._default_manager.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+
+    if user is not None and default_token_generator.check_token(user, token):
+        user.is_active = True
+        user.save()
+        messages.success(request, 'Wow. Your account is now activated.')
+        return redirect('myAccount')
+    else:
+        messages.error(request, 'Invalid activation link.')
+        return redirect('myAccount')
 
 
 def login(request):
