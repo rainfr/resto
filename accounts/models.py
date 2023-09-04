9579dc72c3ea278a8a3433cd581316139c22936d
rain <att4ck07@gmail.com>
2023-09-04 21:16:45 +0200

    another typo error

--- accounts/models.py
+++ accounts/models.py
@@ -89,7 +89,7 @@ class UserProfile(models.Model):
     profile_picture = models.ImageField(upload_to='users/profile_pictures', blank=True, null=True)
     cover_photo = models.ImageField(upload_to='users/cover_photos', blank=True, null=True)
     address_line_1 = models.CharField(max_length=50, blank=True, null=True)
-    adress_line_2 = models.CharField(max_length=50, blank=True, null=True)
+    address_line_2 = models.CharField(max_length=50, blank=True, null=True)
     country = models.CharField(max_length=15, blank=True, null=True)
     state = models.CharField(max_length=15, blank=True, null=True)
     city = models.CharField(max_length=15, blank=True, null=True)
